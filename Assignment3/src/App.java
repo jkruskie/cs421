d840692509da44daf669cba921ffba3f58b088bc
Justin Kruskie <Justin@jkruskie.com>
2022-10-10 22:22:43 -0400

    Implement Seeding & Menu System

--- Assignment3/src/App.java
+++ Assignment3/src/App.java
@@ -2,71 +2,148 @@ import java.io.File;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        if (checkDatabaseExists()) {
-            System.out.println("Database exists");
-            // Create a new instance of the Database
-            Database db = new Database();
-            if (requestDBReset()) {
-                // Initialize the DB first to ensure tables exist
-                db.initialize();
-                // Reset
-                db.reset();
-            } else {
-                // Continue
-            }
-        } else {
+
+   private static boolean continueExec = true;
+   static Database db;
+   static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) throws Exception {
+      if (checkDatabaseExists()) {
+         // System.out.println("Database exists");
+         // Create a new instance of the Database
+         db = new Database();
+         if (requestDBReset()) {
+            // Drop
+            db.drop();
+            // Migrate the DB tables first to ensure they exist
+            db.migrate();
+            // Seed the tables
+            db.seed();
+            // Continue
+            promptUserMenu();
+         } else {
+            // Continue
+            promptUserMenu();
+         }
+      } else {
+         // File does not exist
+         // System.out.println("Database does not exist");
+         // Create a new database file
+         createDatabaseFile();
+
+         // Initialize the database
+         Database db = new Database();
+         db.migrate();
+         db.seed();
+      }
+   }
+
+   private static boolean checkDatabaseExists() {
+      File file = new File("db.sqlite");
+      return file.exists();
+   }
+
+   private static void promptDatabaseReset() {
+      System.out.print("\nWould you like to reset the database? (y/n): ");
+   }
+
+   private static boolean requestDBReset() {
+      // Prompt request to user
+      promptDatabaseReset();
+
+      String response = scanner.nextLine();
+
+      // Check if user input is y or n
+      if (response.equals("y")) {
+         // System.out.println("Requesting resetting of database");
+         return true;
+      } else if (response.equals("n")) {
+         // System.out.println("Not resetting database");
+         return false;
+      } else {
+         System.out.println("Invalid response");
+         return requestDBReset();
+      }
+   }
+
+   public static void createDatabaseFile() {
+      // Create a new file
+      new File("db.sqlite");
+
+      // Confirm that the file was created
+      if (checkDatabaseExists()) {
+         // System.out.println("Database file created");
+      } else {
+         // System.out.println("Database file not created");
+      }
+   }
+
+   public static void promptUserMenu() {
+      // While the continueExec boolean is true, continue to prompt the user
+      // for input regarding which menu option they want.
+      while (continueExec) {
+         System.out.println("\nPlease select an option:");
+         // System.out.println("1. Add a new course");
+         // System.out.println("2. Add a new professor");
+         // System.out.println("3. Add a new classroom");
+         // System.out.println("4. View all courses");
+         // System.out.println("5. View all professors");
+         // System.out.println("6. View all classrooms");
+         System.out.println("1. Import Schedule File");
+         System.out.println("7. Exit");
+         System.out.println("8. Exit & Destroy Database");
+         System.out.print("Enter your choice: ");
+
+         // Get the user input and determine what option they selected
+         int option = scanner.nextInt();
+
+         switch (option) {
+            case 1:
+               // Import Schedule File
+               // System.out.println("Importing schedule file");
+               importScheduleFile();
+               break;
+            case 7:
+               // Exit
+               // System.out.println("Exiting");
+               System.exit(0);
+               break;
+            case 8:
+               // Exit & Destroy Database
+               // System.out.println("Exiting and destroying database");
+               db.drop();
+               deleteDBFile();
+               System.exit(0);
+               break;
+            default:
+               System.out.println("Invalid option");
+               break;
+         }
+      }
+   }
+
+   private static void deleteDBFile() {
+      File file = new File("db.sqlite");
+      file.delete();
+   }
+
+   private static void importScheduleFile() {
+      // Prompt user for file name
+      System.out.print("Enter the absolute file path: ");
+      String response = scanner.next();
+         if(checkFileExists(response)) {
+            // File exists
+            System.out.println("File exists");
+            // Import the file
+         } else {
             // File does not exist
-            System.out.println("Database does not exist");
-            // Create a new database file
-            createDatabaseFile();
-            
-            // Initialize the database
-            Database db = new Database();
-            db.initialize();
-        }
-    }
+            System.out.println("File does not exist");
+            importScheduleFile();
+         }
+   }
 
-    private static boolean checkDatabaseExists() {
-        File file = new File("db.sqlite");
-        return file.exists();
-    }
-
-    private static void promptDatabaseReset() {
-        System.out.println("Would you like to reset the database? (y/n)");
-    }
-
-    private static boolean requestDBReset() {
-        // Prompt request to user
-        promptDatabaseReset();
-
-        // Create new scanner and get user input
-        Scanner scanner = new Scanner(System.in);
-        String response = scanner.nextLine();
-
-        // Check if user input is y or n
-        if (response.equals("y")) {
-            System.out.println("Requesting resetting of database");
-            return true;
-        } else if (response.equals("n")) {
-            System.out.println("Not resetting database");
-            return false;
-        } else {
-            System.out.println("Invalid response");
-            return requestDBReset();
-        }
-    }
-
-    public static void createDatabaseFile()
-    {
-        // Create a new file
-        new File("db.sqlite");
-
-        // Confirm that the file was created
-        if(checkDatabaseExists()) {
-            System.out.println("Database file created");
-        } else {
-            System.out.println("Database file not created");
-        }
-    }
+   private static boolean checkFileExists(String filePath) {
+      File file = new File(filePath);
+      return file.exists();
+   }
 }
--- Assignment3/src/Database.java
+++ Assignment3/src/Database.java
@@ -5,113 +5,202 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:db.sqlite";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
+   private Connection connect() {
+      // SQLite connection string
+      String url = "jdbc:sqlite:db.sqlite";
+      Connection conn = null;
+      try {
+         conn = DriverManager.getConnection(url);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+      return conn;
+   }
 
-    public void initialize() {
-        String sql = "CREATE TABLE IF NOT EXISTS schedule_table (\n"
-                + "	tuid integer PRIMARY KEY,\n"
-                + "	course_tuid text NOT NULL,\n"
-                + "	classroom_tuid text NOT NULL,\n"
-                + "	professor_tuid text NOT NULL,\n"
-                + "	section integer NOT NULL,\n"
-                + "	start_time text NOT NULL,\n"
-                + "	end_time text NOT NULL,\n"
-                + "	days text NOT NULL);";
+   public void migrate() {
+      String sql = "CREATE TABLE IF NOT EXISTS schedule_table (\n"
+            + "	tuid integer PRIMARY KEY,\n"
+            + "	course_tuid text NOT NULL,\n"
+            + "	classroom_tuid text NOT NULL,\n"
+            + "	professor_tuid text NOT NULL,\n"
+            + "	section integer NOT NULL,\n"
+            + "	start_time text NOT NULL,\n"
+            + "	end_time text NOT NULL,\n"
+            + "	days text NOT NULL);";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Creating schedule table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Creating schedule table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
 
-        sql = "CREATE TABLE IF NOT EXISTS professors_table (\n"
-                + "	tuid integer PRIMARY KEY,\n"
-                + "	professor_name text NOT NULL);";
+      sql = "CREATE TABLE IF NOT EXISTS professors_table (\n"
+            + "	tuid integer PRIMARY KEY,\n"
+            + "	professor_name text NOT NULL);";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Creating professors table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Creating professors table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
 
-        sql = "CREATE TABLE IF NOT EXISTS classroom_table (\n"
-                + "	tuid integer PRIMARY KEY,\n"
-                + "	classroom_name text NOT NULL,\n"
-                + " capacity integer NOT NULL);";
+      sql = "CREATE TABLE IF NOT EXISTS classroom_table (\n"
+            + "	tuid integer PRIMARY KEY,\n"
+            + "	classroom_name text NOT NULL,\n"
+            + " capacity integer NOT NULL);";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Creating classroom table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Creating classroom table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
 
-        sql = "CREATE TABLE IF NOT EXISTS courses_table (\n"
-                + "	tuid integer PRIMARY KEY,\n"
-                + "	course_id text NOT NULL,\n"
-                + "	course_title text NOT NULL,\n"
-                + "	credit_hours integer NOT NULL);";
+      sql = "CREATE TABLE IF NOT EXISTS courses_table (\n"
+            + "	tuid integer PRIMARY KEY,\n"
+            + "	course_id text NOT NULL,\n"
+            + "	course_title text NOT NULL,\n"
+            + "	credit_hours integer NOT NULL);";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Creating courses table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Creating courses table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+   }
 
-    public void reset() {
-        String sql = "DELETE FROM schedule_table;";
+   public void drop() {
+      String sql = "DROP TABLE IF EXISTS schedule_table;";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Resetting schedule table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Dropping schedule table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
 
-        sql = "DELETE FROM professors_table;";
+      sql = "DROP TABLE IF EXISTS professors_table;";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Resetting professors table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Dropping professors table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
 
-        sql = "DELETE FROM courses_table;";
+      sql = "DROP TABLE IF EXISTS courses_table;";
 
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement()) {
-            // create a new table
-            System.out.println("Resetting courses table...");
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Dropping courses table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+
+      sql = "DROP TABLE IF EXISTS classroom_table;";
+
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Dropping classroom table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+   }
+
+   public void seed() {
+      // Seed classrooms
+      String sql = "INSERT INTO classroom_table(classroom_name, capacity)\n"
+      + " VALUES('A', 30),\n"
+      + " ('B', 25),\n"
+      + " ('C', 20);";
+
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Seeding classroom table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+
+      // Seed courses
+      sql = "INSERT INTO courses_table(course_id, course_title, credit_hours)\n"
+      + " VALUES('CS 101', 'Intro to Computer Science', 3),\n"
+      + " ('CS 105', 'Computers and Programming', 4),\n"
+      + " ('CSC 105', 'Computers and Programming', 4), \n"
+      + " ('CSC 107', 'Introduction to Code Preparation', 1), \n"
+      + " ('CSC 116', 'Programming I', 4), \n"
+      + " ('CSC 216', 'Programming II', 4), \n"
+      + " ('CSC 227', 'Commenting and Naming Conventions', 2), \n"
+      + " ('CSC 316', 'Data Structures & Algorithms', 4), \n"
+      + " ('CSC 416', 'Advanced Algorithm Analysis', 3), \n"
+      + " ('CSC 211', 'Introductory .NET Development', 3), \n"
+      + " ('CSC 311', 'Advanced .NET Development', 4), \n"
+      + " ('CSC 313', 'Real World Application Development', 3), \n"
+      + " ('CSC 411', 'Data Driven Systems', 3), \n"
+      + " ('CSC 412', 'Sensor Systems', 3), \n"
+      + " ('CSC 413', 'Artificial Intelligence Systems', 3), \n"
+      + " ('CSC 496', 'Software Engineering I', 4), \n"
+      + " ('CSC 497', 'Software Engineering II', 4), \n"
+      + " ('CSC 498', 'Software Engineering III', 4);";
+
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Seeding courses table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+
+      // Seed professors
+      sql = "INSERT INTO professors_table(professor_name)\n"
+      + " VALUES('james'),\n"
+      + " ('smith'),\n"
+      + " ('jones'),\n"
+      + " ('vasques'),\n"
+      + " ('abdul'),\n"
+      + " ('thomas');";
+
+      try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement()) {
+         // create a new table
+         // System.out.println("Seeding professors table...");
+         stmt.execute(sql);
+      } catch (SQLException e) {
+         // System.out.println(e.getMessage());
+      }
+   }
+
+   public void close()
+   {
+      // End connection to database
+      Connection conn = this.connect();
+      try {
+         if (conn != null) {
+            conn.close();
+         }
+      } catch (SQLException ex) {
+         // System.out.println(ex.getMessage());
+      }
+   }
 }
